212cdfe5f761c0e1872d9526660d82fcd049c027
Matheus Quevedo <matheusq10@gmail.com>
2023-11-11 12:52:06 -0300

    Refatoração da lógica then

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
+async function buscarEMostrarVideos() {
+    const busca = await fetch("http://localhost:3000/videos")
+    const videos = await busca.json();
 
-    .then(res => res.json())
-    .then((videos) =>
-        videos.forEach((video) => {
-            containerVideos.innerHTML += `
+    videos.forEach((video) => {
+        containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
             <div class="descricao-video">
@@ -15,10 +15,7 @@ const api = fetch("http://localhost:3000/videos")
             </div>
         </li>
             `;
-        })
-    )
-
-    .catch((error) => {
-        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`
-
     })
+}
+
+buscarEMostrarVideos();
